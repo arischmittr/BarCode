0c60039387a460d600cb74bf857981a476d252db
ariana <89927047+arischmittr@users.noreply.github.com>
2022-03-27 00:27:24 -0300

    Update index.cpp
    Código alterado.

--- index.cpp
+++ index.cpp
@@ -1,11 +1,12 @@
-// Basic declarations.
+//Declarações básicas.
 #include <iostream>
 
 using namespace std;
 
-// Color declarations
-#define NC "\033[0m"        /* No Color */
-#define BRED "\033[31m"     /* Bold Red */
+// Declaração de cores.
+
+#define NC "\033[0m"   /* No color */
+#define BRED "\033[31m" /* Bold Red */
 #define BGREEN "\033[32m"   /* Bold Green */
 #define BYELLOW "\033[33m"  /* Bold Yellow */
 #define BBLUE "\033[34m"    /* Bold Blue */
@@ -15,81 +16,84 @@ using namespace std;
 
 int main()
 {
-    // Define Varibles
+    // Definição de variaveis.
+
     long long int Barcode;
 
-    int d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, dv, soma, mult, length;
+    int d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, dv, soma, mult, comprimento;
 
-    length = 0;
-    cout << BYELLOW << "Let's verify your Barcode! \n\n"
+    comprimento = 0;
+
+    cout << BYELLOW << "Vamos verificar seu Barcode! \n\n"
          << NC;
-    cout << BYELLOW << "Insert your Barcode \n"
+    cout << BYELLOW << "Insira seu Barcode\n"
          << NC;
     cin >> Barcode;
 
-    // Takes user input and separete digits
-    d0 = Barcode / 1000000000000;
-    d1 = (Barcode / 100000000000) % 10;
+    // Recebe comandos de entrada do usuário e separa os digitos.
+
+    d0 = Barcode/ 1000000000000 ;
+    d1 = (Barcode / 100000000000) % 10 ;
     d2 = ((Barcode / 10000000000) % 100) % 10;
     d3 = (((Barcode / 1000000000) % 1000) % 100) % 10;
-    d4 = ((((Barcode / 100000000) % 10000) % 1000) % 100) % 10;
-    d5 = (((((Barcode / 10000000) % 100000) % 10000) % 1000) % 100) % 10;
+    d4 =((((Barcode / 100000000) % 10000) % 1000) % 100) % 10;
+    d5 = (((((Barcode/ 10000000) % 100000) % 10000) % 1000) % 100) % 10;
     d6 = ((((((Barcode / 1000000) % 1000000) % 100000) % 10000) % 1000) % 100) % 10;
-    d7 = (((((((Barcode / 100000) % 10000000) % 1000000) % 100000) % 10000) % 1000) % 100) % 10;
-    d8 = ((((((((Barcode / 10000) % 100000000) % 10000000) % 1000000) % 100000) % 10000) % 1000) % 100) % 10;
-    d9 = (((((((((Barcode / 1000) % 1000000000) % 100000000) % 10000000) % 1000000) % 100000) % 10000) % 1000) % 100) % 10;
-    d10 = ((((((((((Barcode / 100) % 10000000000) % 1000000000) % 100000000) % 10000000) % 1000000) % 100000) % 10000) % 1000) % 100) % 10;
-    d11 = (((((((((((Barcode / 10) % 100000000000) % 10000000000) % 1000000000) % 100000000) % 10000000) % 1000000) % 100000) % 10000) % 1000) % 100) % 10;
-    dv = ((((((((((((Barcode % 1000000000000) % 100000000000) % 10000000000) % 10000000000) % 1000000000) % 100000000) % 10000000) % 1000000) % 100000) % 10000) % 1000) % 100) % 10;
+    d7 = (((((((Barcode/ 100000) % 10000000) % 1000000) % 100000) % 10000) % 1000) % 100) % 10;
+    d8 = ((((((((Barcode/ 10000) % 100000000) % 10000000) % 1000000) % 100000) % 10000) % 1000) % 100) % 10;
+    d9 =(((((((((Barcode/ 1000) % 1000000000) % 100000000) % 10000000) % 1000000) % 100000) % 10000) % 1000) % 100) % 10;
+    d10 = ((((((((((Barcode/ 100) % 10000000000) % 1000000000) % 100000000) % 10000000) % 1000000) % 100000) % 10000) % 1000) % 100) % 10;
+    d11 = (((((((((((Barcode/ 10) % 100000000000) % 10000000000) % 1000000000) % 100000000) % 10000000) % 1000000) % 100000) % 10000) % 1000) % 100) % 10;
+    dv = ((((((((((((Barcode/ 1000000000000% 1000000000000) % 100000000000) % 10000000000) % 10000000000) % 1000000000) % 100000000) % 10000000) % 1000000) % 100000) % 10000) % 1000) % 100) % 10;
 
-    // Print out User Barcode
+    // Imprime o código de barras do usuário.
 
-    cout << BWHITE << "\nYour Barcode is \t" << Barcode << NC << endl;
+    cout << BWHITE << "\nSeu Barcode é \t" << Barcode << NC << endl;
 
-    // Make the mathematical operations to calculate the verification digit
+    // Faça as operações matemáticas para calcular o dígito de verificação.
 
     soma = d0 + d1 * 3 + d2 + d3 * 3 + d4 + d5 * 3 + d6 + d7 * 3 + d8 + d9 * 3 + d10 + d11 * 3;
 
     mult = (soma / 10 + 1) * 10;
 
-    // get lebght of integer
+    // Obter o comprimento do inteiro.
 
     do
     {
-        ++length;
+        ++comprimento;
         Barcode /= 10;
-    } while (Barcode != 0);
+    } while (Barcode!= 0);
 
-    // Check if User Barcode has 13 Integers and if the verification digit is correct
-    if (length == 13 && mult - soma == dv)
+    // Verifique se o código de barras do usuário tem 13 inteiros e se o dígito de verificação está correto.
+
+    if (comprimento == 13 && mult - soma == dv)
     {
-        cout << BGREEN << "Your Barcode is Valid\t" << NC;
+        cout << BGREEN << "Seu Barcode é valido\t" << NC;
     }
 
-    // Check if it has the proprer lenght and if the verification digit is not correct
+    // Verifique se tem o comprimento adequado e se o dígito de verificação não está correto.
 
-    else if (length == 13 && mult - soma != dv)
+    else if (comprimento == 13 && mult - soma != dv)
     {
-        cout << BMAGENTA << "Your Barcode is Valid, but the Verify Digit is Wrong\t" << NC;
+        cout << BMAGENTA << "Seu Barcode é válido, mas o dígito de verificação está errado\t" << NC;
     }
 
-    // Check lenght for User response
+    // Verifique o comprimento da resposta do usuário.
 
-    else if (length <= 13)
+    else if (comprimento <= 13)
     {
-        cout << BRED << "Your Barcode is to short\t" << NC;
+        cout << BRED << "Seu Barcode é muito curto\t" << NC;
     }
 
-    else if (length >= 13)
+    else if (comprimento >= 13)
     {
-        cout << BRED << "Your Barcode is to long\t" << NC;
+        cout << BRED << "Seu Barcode muito longo\t" << NC;
     }
 
-    // if none of the above checks catch a mistake it reproves the barcode
+    //Se nenhuma das verificações acima detectar um erro, ele reprova o código de barras.
 
     else
     {
-        cout << BRED << "Your Barcode is not Valid\t" << NC;
+        cout << BRED << "Seu Barcode não é valido\t" << NC;
     }
     return 0;
-}
